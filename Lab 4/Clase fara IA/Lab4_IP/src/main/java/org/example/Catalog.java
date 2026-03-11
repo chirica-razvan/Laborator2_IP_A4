fd3e88f2805e40167185e5416eafa60e99cdfad8
SfGraal <chirica.razvan100@gmail.com>
2026-03-11 18:33:24 +0200

    Catalog began prototype class

---
+++ Lab 4/Clase fara IA/Lab4_IP/src/main/java/org/example/Catalog.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+public class Catalog {
+    List<Product,Integer price> productList = new ArrayList<>();
+
+
+}
